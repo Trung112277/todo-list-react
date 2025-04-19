698f92f8a2d327cd9abf712d73a7438c43eacd76
Trung112277 <nhattrung112277@GMAIL.COM>
2025-04-19 10:55:52 +0700

    checkbox ui

--- src/components/pages/designSystem/index.tsx
+++ src/components/pages/designSystem/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox"
 
 export function PageDesignSystem() {
   return (
@@ -9,17 +10,20 @@ export function PageDesignSystem() {
         <div className="w-1/2 flex flex-col gap-4">
           <h2 className="text-4xl font-semibold text-center text-primary">Atoms</h2>
           <div className="flex gap-5 items-center">
-            <h3 className="text-3xl font-medium min-w-32">Button:</h3>
+            <h3 className="text-3xl font-medium min-w-40">Button:</h3>
           <Button>Button</Button>
           </div>
           <div className="flex gap-5 items-center">
-            <h3 className="text-3xl font-medium min-w-32">Input:</h3>
+            <h3 className="text-3xl font-medium min-w-40">Input:</h3>
             <Input placeholder="text..." />
+          </div>
+          <div className="flex gap-5 items-center">
+            <h3 className="text-3xl font-medium min-w-40">Checkbox:</h3>
+            <Checkbox />
           </div>
         </div>
         <div className="w-1/2 flex flex-col gap-4">
         <h2 className="text-4xl font-semibold text-center text-primary">Molecules</h2>
-
         </div>
       </div>
 
