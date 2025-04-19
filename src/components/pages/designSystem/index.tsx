c0980d43708edbea8c4d17234c3e4b98b6f6527d
Trung112277 <nhattrung112277@GMAIL.COM>
2025-04-19 11:48:57 +0700

    select ui

--- src/components/pages/designSystem/index.tsx
+++ src/components/pages/designSystem/index.tsx
@@ -2,6 +2,14 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 
 
 export function PageDesignSystem() {
@@ -9,7 +17,7 @@ export function PageDesignSystem() {
     <div className="container m-auto p-5 font-sans">
       <h1 className="text-6xl font-bold text-center text-primary">Design System</h1>
       <div className="flex gap-[50px] my-10">
-        <div className="w-1/2 flex flex-col gap-4">
+        <div className="w-1/2 flex flex-col gap-6">
           <h2 className="text-4xl font-semibold text-center text-primary">Atoms</h2>
           <div className="flex gap-5 items-center">
             <h3 className="text-3xl font-medium min-w-40">Button:</h3>
@@ -27,8 +35,21 @@ export function PageDesignSystem() {
             <h3 className="text-3xl font-medium min-w-40">Label:</h3>
             <Label >This is Label</Label>
           </div>
+          <div className="flex gap-5 items-center">
+            <h3 className="text-3xl font-medium min-w-40">Select:</h3>
+            <Select defaultValue="select-2">
+              <SelectTrigger className="w-[200px]">
+                <SelectValue/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="select-1">Select 1</SelectItem>
+                <SelectItem value="select-2">Select 2</SelectItem>
+                <SelectItem value="select-3">Select 3</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
-        <div className="w-1/2 flex flex-col gap-4">
+        <div className="w-1/2 flex flex-col gap-6">
         <h2 className="text-4xl font-semibold text-center text-primary">Molecules</h2>
         </div>
       </div>
