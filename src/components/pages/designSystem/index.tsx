6b435b3c29e5437ab44f2fc6bf4b5d7d3c15bb3d
Trung112277 <nhattrung112277@GMAIL.COM>
2025-04-19 09:48:11 +0700

    design system update

--- src/components/pages/designSystem/index.tsx
+++ src/components/pages/designSystem/index.tsx
@@ -4,7 +4,21 @@ export function PageDesignSystem() {
   return (
     <div className="container m-auto p-5 font-sans">
       <h1 className="text-6xl font-bold text-center text-primary">Design System</h1>
-      <Button>Button</Button>
+      <div className="flex gap-[50px] my-10">
+        <div className="w-1/2 flex flex-col gap-4">
+          <h2 className="text-4xl font-semibold text-center text-primary">Atoms</h2>
+          <div className="flex gap-5 items-center">
+            <h3 className="text-3xl font-medium">Button:</h3>
+          <Button>Button</Button>
+          </div>
+
+        </div>
+        <div className="w-1/2 flex flex-col gap-4">
+        <h2 className="text-4xl font-semibold text-center text-primary">Molecules</h2>
+
+        </div>
+      </div>
+
     </div>
   );
 }
