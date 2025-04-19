3be1786f8507ccb7559db3aa109c294e23e9d4f1
Trung112277 <nhattrung112277@GMAIL.COM>
2025-04-19 11:07:45 +0700

    label ui

--- src/components/pages/designSystem/index.tsx
+++ src/components/pages/designSystem/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+
 
 export function PageDesignSystem() {
   return (
@@ -20,6 +22,10 @@ export function PageDesignSystem() {
           <div className="flex gap-5 items-center">
             <h3 className="text-3xl font-medium min-w-40">Checkbox:</h3>
             <Checkbox />
+          </div>
+          <div className="flex gap-5 items-center">
+            <h3 className="text-3xl font-medium min-w-40">Label:</h3>
+            <Label >This is Label</Label>
           </div>
         </div>
         <div className="w-1/2 flex flex-col gap-4">
